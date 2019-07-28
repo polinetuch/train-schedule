c92b638f0ce9fc7fe57299be8f85b79478e5275a
unknown <uchpolinet@gmail.com>
2019-07-28 15:29:35 +1000

    linked html file to jquery, javascript, firebase and css

---
+++ javascript/train-schedule.js
@@ -0,0 +1,12 @@
+// Web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAn-dOQYlVkZEbUYT27XeSehkt_kAfm9Wg",
+  authDomain: "train-schedule-1b4d2.firebaseapp.com",
+  databaseURL: "https://train-schedule-1b4d2.firebaseio.com",
+  projectId: "train-schedule-1b4d2",
+  storageBucket: "",
+  messagingSenderId: "216924017587",
+  appId: "1:216924017587:web:b03a79a7e602d0a9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
